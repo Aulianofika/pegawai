ee3d6d526b226999ba8337faf8c4d8469fe2bcd1
Aulia Nofika <aulianofika514@gmail.com>
2025-06-19 19:56:27 +0700

    Create pegawaii_dosen.sql

---
+++ pegawaii_dosen.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 19, 2025 at 02:55 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pegawaii_dosen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dosens`
+--
+
+CREATE TABLE `dosens` (
+  `no` int(10) UNSIGNED NOT NULL,
+  `nip` varchar(255) NOT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
+  `no_telepon` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `dosens`
+--
+
+INSERT INTO `dosens` (`no`, `nip`, `nama_lengkap`, `no_telepon`, `email`, `alamat`, `created_at`, `updated_at`) VALUES
+(2, '6599', 'Aulianofika2', '081275549631', 'Aulia@gmail.com', 'jl.tuangadang', '2025-06-13 03:06:04', '2025-06-13 03:06:04'),
+(4, '444', 'pkka44444', '4444', 'aul@gmail.com', 'yyyuj6', '2025-06-19 11:52:15', '2025-06-19 11:57:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2025_06_13_020439_create_dosens_table', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dosens`
+--
+ALTER TABLE `dosens`
+  ADD PRIMARY KEY (`no`),
+  ADD UNIQUE KEY `dosens_nip_unique` (`nip`),
+  ADD UNIQUE KEY `dosens_email_unique` (`email`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dosens`
+--
+ALTER TABLE `dosens`
+  MODIFY `no` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
